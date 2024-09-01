3c227fc88703f44f710a0ea0eb12acef7a939bd9
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-08-31 19:14:08 -0700

    Added Unit Tests

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,12 @@
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
 class TextNode:
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url = None):
         self.text = text
         self.text_type = text_type
         self.url = url
