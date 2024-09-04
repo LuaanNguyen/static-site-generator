d3bef000fc091bb30a71fbb036962a9849b13d6d
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-09-04 15:41:12 -0700

    Added constructor to HTMLNode class

---
+++ src/htmlnode.py
@@ -0,0 +1,7 @@
+class HTMLNode: 
+    def __init__(self, tag = None, value = None, children = None, props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props 
+        
