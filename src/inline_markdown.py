3ee33f1d449f6fa8145d0d312623e9aa69bc6db3
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-09-25 23:38:05 -0700

    Fix: Typo in Exception checking

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -9,23 +9,21 @@ from textnode import (
 )
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_nodes = [] 
+    new_nodes = []
     for old_node in old_nodes:
         if old_node.text_type != text_type_text:
             new_nodes.append(old_node)
+            continue
         split_nodes = []
         sections = old_node.text.split(delimiter)
-        if len(sections) & 2 == 0:
-            raise ValueError("Invalid mardown, formatted section not closed")
-        
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
         for i in range(len(sections)):
             if sections[i] == "":
-                continue 
+                continue
             if i % 2 == 0:
                 split_nodes.append(TextNode(sections[i], text_type_text))
             else:
                 split_nodes.append(TextNode(sections[i], text_type))
-                
         new_nodes.extend(split_nodes)
     return new_nodes
-    
